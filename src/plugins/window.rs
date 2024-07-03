aaff3c3c5aa9428477b88b0e65ce8031a70e3abe
Implycitt <qgbordelon@gmail.com>
2024-07-02 23:20:38 -0500

    Application

--- src/plugins/window.rs
+++ src/plugins/window.rs
@@ -1,7 +1,8 @@
 use iced::{
+    executor,
     alignment::{Horizontal, Vertical},
-    widget::{column, container, Container},
-    Length, Sandbox, Settings, Theme,
+    widget::{column, container, Container, horizontal_space, row, text, text_editor},
+    {Length, Settings, Theme, Element, Command, Application},
 };
 
 pub fn create_window() -> iced::Result {
@@ -10,19 +11,22 @@ pub fn create_window() -> iced::Result {
 
 struct Window;
 
-impl Sandbox for Window {
+impl Application for Window {
+    type Executor = executor::Default;
+    type Flags = ();
     type Message = ();
+    type Theme = Theme;
 
-    fn new() -> Self {
-        Self
+    fn new(_flags: ()) -> (Window, Command<Self::Message>) {
+        (Window, Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Language")
     }
 
-    fn update(&mut self, _message: Self::Message) {
-        todo!();
+    fn update(&mut self, _message: Self::Message)  -> Command<Self::Message> {
+        Command::none()
     }
 
     fn view(&self) -> iced::Element<Self::Message> {
